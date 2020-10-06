4e0da0ba8a0353f6a6c01fe16dab317d2694a3cc
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-06 23:01:13 +0300

    Lesson finish

--- App.js
+++ App.js
@@ -9,34 +9,10 @@ import {
   Text,
   StyleSheet,
   TextInput,
-  Platform,
+  TouchableOpacity,
 } from 'react-native';
 
 const App = () => {
-  function sayHello() {
-    const platform = Platform.OS;
-    const version = Platform.Version;
-
-    if (platform === 'ios')
-      return (
-        <Text
-          style={{
-            fontSize: 30,
-          }}>
-          Hello to iPhone: {version}
-        </Text>
-      );
-    else
-      return (
-        <Text
-          style={{
-            fontSize: 30,
-          }}>
-          Hello to Android! {version}
-        </Text>
-      );
-  }
-
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
@@ -46,8 +22,10 @@ const App = () => {
             placeholder="Aramak istediğiniz ürünü giriniz.."
           />
         </View>
-        {/* <Text style={{fontSize: 40}}>Cihaz: {platform} </Text>  */}
-        {sayHello()}
+
+        <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.textStyle}>Ara</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -59,6 +37,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#e0e0e0',
+    justifyContent: 'center'
   },
   inputContainer: {
     backgroundColor: 'white',
@@ -68,4 +47,16 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
   },
+  buttonContainer: {
+    backgroundColor: 'blue', 
+    padding: 10,
+    margin: 20,
+    borderRadius: 5,
+    alignItems: 'center'
+  },
+  textStyle: {
+    fontSize: 17,
+    color: 'white',
+    fontWeight: 'bold'
+  }
 });
