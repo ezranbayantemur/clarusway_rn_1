aea70437cfdb1699fb80f8b3c31a392a0efea109
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-06 21:37:06 +0300

    Flexbox justify&align

--- App.js
+++ App.js
@@ -3,30 +3,28 @@
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView style={{backgroundColor: 'green', flex: 1}}>
-      <View style={{backgroundColor: 'red', flex: 1}}>
-        <Text>Merhaba</Text>
-        <Text>Merhaba</Text>
-        <Text>Merhaba</Text>
-      </View>
-
-      <View style={{backgroundColor: 'blue', flex: 1}}>
-        <Text>Selam</Text>
-      </View>
-
-      <View style={{backgroundColor: 'yellow', flex: 1}}>
-        <Text>Clarusway</Text>
-        <Text>Clarusway</Text>
-        <Text>Clarusway</Text>
-        <Text>Clarusway</Text>
-        <Text>Clarusway</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <View style={{width: 40, height: 40, backgroundColor: 'red'}} />
+        <View style={{width: 40, height: 40, backgroundColor: 'blue'}} />
+        <View style={{width: 40, height: 40, backgroundColor: 'green'}} />
       </View>
     </SafeAreaView>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#e0e0e0',
+    justifyContent: 'center', // 'space-between', 'space-around', 'space-evenly',
+    alignItems: 'flex-start', // flex-start, flex-end
+  },
+});
