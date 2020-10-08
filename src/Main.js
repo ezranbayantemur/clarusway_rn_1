15808f48da2d376d8cf365907e978a64f2f3a61a
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-08 20:24:44 +0300

    Changed src

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+    return(
+        <SafeAreaView style={{flex: 1}}>
+            <View>
+                <Text>Hello React Native!</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default App;
