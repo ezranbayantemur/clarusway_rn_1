5476a287d15e1717e9dff6d0ef99d9dfb7d54e4f
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-08 22:24:26 +0300

    Dummy data

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,36 @@ import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
 
 import MyBanner from './components/MyBanner';
 
-
+const food_data = [
+  {
+    id: 0,
+    title: 'Çorbalar',
+    desc: 'Birbirinden leziz çorbalar!',
+    color: '#e57373',
+    isActive: false,
+  },
+  {
+    id: 1,
+    title: 'Ara Sıcaklar',
+    desc: 'Lezzetlei aparetifler',
+    color: '#81d4fa',
+    isActive: true,
+  },
+  {
+    id: 2,
+    title: 'Ana Yemekler',
+    desc: 'Doyurucu lezzetler',
+    color: '#ffd54f',
+    isActive: false,
+  },
+  {
+    id: 3,
+    title: 'Alkolsüz İçecekler',
+    desc: 'Buz gibi serinletici lezzetler',
+    color: '#cfd8dc',
+    isActive: false,
+  },
+];
 
 const App = () => {
   return (
