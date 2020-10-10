6bf41f76fb29a7047dc1261ddb17844ae8abe3de
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-10 17:45:17 +0300

    Logo added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Login from './src/Login'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Login);
---
+++ src/Login.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { SafeAreaView, View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+
+const Login = () => {
+    const dimension = Dimensions.get('window');
+    console.log(dimension);
+
+    return (
+        <SafeAreaView>
+            <View>
+                <Image
+                    source={require('./assets/cart.png')}
+                    style={styles.logoStyle}
+                />
+                <Text>Login</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+    logoStyle: {
+        backgroundColor: 'red',
+        resizeMode: 'contain',
+        width: Dimensions.get('window').width,
+    }
+})
