b3b8905c444e293eb31f1c9a05a153d058dc2a3f
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-10 19:46:45 +0300

    .map vs FlatList

--- src/News.js
+++ src/News.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, ScrollView, FlatList } from 'react-native';
+
+const myArray = Array.from(Array(15000).keys());
 
 const News = () => {
     return (
-        <SafeAreaView>
-            <View>
-                <Text>News</Text>
-            </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            {/* <ScrollView style={{ flex: 1 }}>
+                {
+                    myArray.map(u => <Text style={{ fontSize: 30 }}>{u}</Text>)
+                }
+            </ScrollView> */}
+
+            <FlatList
+                data={myArray}
+                renderItem={(data) => <Text style={{ fontSize: 30 }}>{data.item}</Text>}
+            />
+
+
         </SafeAreaView>
     )
 }
