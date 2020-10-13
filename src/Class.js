bdad4a99d5a66557d44d82fb78bc696dd46e9e7f
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-13 21:46:11 +0300

    State initialized

--- src/Class.js
+++ src/Class.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, Button } from 'react-native';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            counter: 0
+        }
     }
 
     render() {
         return (
             <SafeAreaView>
                 <View>
-                    <Text>Merhaba</Text>
+                    <Text style={{ fontSize: 250 }}>{this.state.counter}</Text>
+
+                    <Button
+                        title="Up!"
+                        onPress={() => this.setState({
+                            counter: this.state.counter + 1
+                        })}
+                    />
                 </View>
             </SafeAreaView>
         )
--- src/Functional.js
+++ src/Functional.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, Button } from 'react-native';
 
 const App = (props) => {
+    //       var,   function
+    const [counter, setCounter] = useState(0);
+
     return (
         <SafeAreaView>
             <View>
-                <Text>Merhaba</Text>
+                <Text style={{ fontSize: 250 }}>{counter}</Text>
+
+                <Button
+                    title="Up!"
+                    onPress={() => setCounter(counter + 1)}
+                />
             </View>
         </SafeAreaView>
     )
