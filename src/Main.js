408b08cb647fa8a2257f6d9814476c8c1ba01f09
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-16 20:09:14 +0300

    Overall - 1

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Hello 🌍</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default App;
