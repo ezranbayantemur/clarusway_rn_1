24818e44792207f7423a90926735ce648a901233
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-17 20:46:50 +0300

    Main page created

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,56 @@
 // Overall - 1
-import React from 'react';
-import { SafeAreaView, View, Text, Alert } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, Text, FlatList, StyleSheet, Alert, TextInput } from 'react-native';
+import proudctData from './product_data.json'
 
-import MyInput from './components/MyInput';
+import { ProductCard } from './components'
 
 const App = () => {
+    const [searchValue, setSearchValue] = useState("");
+
+    const renderListItem = ({ item }) => <ProductCard product={item} />
+
+    useEffect(() => {
+        // Alert.alert("Clarushop", "Hoşgeldiniz, keyifli alışverişler..");
+    }, [])
+
+
     return (
         <SafeAreaView>
             <View>
-                <Text style={{ fontSize: 50 }}>Hello there</Text>
+                <Text style={styles.banner}>Clarushop</Text>
 
-                <MyInput
-                    title="Set Name"
-                    getName={(myValue) => alert("Girilen değer: " + myValue)}
+                <View style={styles.searchBar}>
+                    <TextInput
+                        placeholder="Ürün ara..."
+                        onChangeText={(value) => setSearchValue(value)}
+                    />
+                </View>
+
+                <FlatList
+                    keyExtractor={(_, index) => index.toString()}
+                    data={proudctData}
+                    renderItem={renderListItem}
+                    numColumns={2}
                 />
-
             </View>
         </SafeAreaView>
     )
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+    banner: {
+        color: 'purple',
+        fontWeight: 'bold',
+        fontSize: 40,
+        textAlign: 'center'
+    },
+    searchBar: {
+        backgroundColor: "#eceff1",
+        padding: 8,
+        margin: 5,
+        borderRadius: 10
+    }
+})
