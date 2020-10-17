6b467f5c7a4fd12835ed78dc2bae62df01c710d4
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-17 17:35:04 +0300

    Function passing

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,15 @@
 // Overall - 1
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, Alert } from 'react-native';
 
 import Button from './components/Button';
 
 const App = () => {
+
+    function sayHello() {
+        alert("Hello there!")
+    }
+
     return (
         <SafeAreaView>
             <View>
@@ -12,19 +17,19 @@ const App = () => {
             </View>
 
             <Button
-                users={[1, 2, 3, 4, 5]}
-                count={189}
-                number={3}
+                sayingHello={() => sayHello()}
                 color="#81d4fa"
                 banner="Press me!!"
             />
 
             <Button
-                users={[1, 2, 3, 4, 5]}
-                count={189}
-                number={3}
-                banner="Press me!!"
+                sayingHello={() => {
+                    Alert.alert("Clarusway", "Fullstack 4ever!")
+                }}
+                color="#81d4fa"
+                banner="Press me and say hi!!"
             />
+
 
         </SafeAreaView>
     )
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,16 @@ import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const Button = (props) => {
     return (
-        <TouchableOpacity style={
-            [
-                myStyles.buttonContainer,
-                {
-                    backgroundColor: props.color === undefined ? "#a5d6a7" : props.color
-                }
-            ]}>
+        <TouchableOpacity
+            style={
+                [
+                    myStyles.buttonContainer,
+                    {
+                        backgroundColor: props.color === undefined ? "#a5d6a7" : props.color
+                    }
+                ]}
+            onPress={props.sayingHello}
+        >
             <Text style={myStyles.textStyle}>{props.banner}</Text>
         </TouchableOpacity>
     )
