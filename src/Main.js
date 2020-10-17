969bcf1932c4d4084ed5db2d28640c48c9b846ec
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-17 21:06:24 +0300

    Lesson completed..

--- src/Main.js
+++ src/Main.js
@@ -7,17 +7,29 @@ import { ProductCard } from './components'
 
 const App = () => {
     const [searchValue, setSearchValue] = useState("");
+    const [displayList, setDisplayList] = useState([])
 
     const renderListItem = ({ item }) => <ProductCard product={item} />
 
     useEffect(() => {
         // Alert.alert("Clarushop", "Hoşgeldiniz, keyifli alışverişler..");
+        setDisplayList(proudctData)
     }, [])
 
+    useEffect(() => {
+        const filteredValue = proudctData.filter(item => {
+            const text = searchValue.toUpperCase();
+            const productTitle = item.title.toUpperCase();
+
+            return productTitle.indexOf(text) > -1;
+        })
+        setDisplayList(filteredValue)
+
+    }, [searchValue])
 
     return (
-        <SafeAreaView>
-            <View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
                 <Text style={styles.banner}>Clarushop</Text>
 
                 <View style={styles.searchBar}>
@@ -29,7 +41,7 @@ const App = () => {
 
                 <FlatList
                     keyExtractor={(_, index) => index.toString()}
-                    data={proudctData}
+                    data={displayList}
                     renderItem={renderListItem}
                     numColumns={2}
                 />
@@ -37,6 +49,11 @@ const App = () => {
         </SafeAreaView>
     )
 }
+
+/**
+ * 1) Stok durumunu belirtmeniz
+ * 2) <SearchBar onSearch={...} />
+ */
 
 export default App;
 
