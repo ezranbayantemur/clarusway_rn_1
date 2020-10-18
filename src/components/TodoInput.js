01fe6ee4fb72cc4f035135247057f2cb74dd8f93
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-18 16:48:32 +0300

    TodoInput component created

---
+++ src/components/TodoInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+const TodoInput = (props) => {
+    return (
+        <View>
+            <Text>Todo Input</Text>
+        </View>
+    )
+}
+
+export default TodoInput;
