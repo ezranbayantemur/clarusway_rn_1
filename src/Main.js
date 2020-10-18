ed159fe207a052196776151fa9dbcee5dd7df477
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-18 18:51:45 +0300

    Todo List state created

--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, View, KeyboardAvoidingView, FlatList } from 'react-native';
 
 import { main } from './styles';
 import TodoInput from './components/TodoInput';
 
 const Main = () => {
+    const [list, setList] = useState([])
+
+    function addTodo(text) {
+        const element = {
+            id: list.length,
+            todo: text,
+            isDone: false
+        }
+        list.p
+        const newArray = [...list]
+        newArray.push(element)
+
+        setList(newArray);
+    }
+
     return (
         <SafeAreaView style={main.container}>
-            <View style={main.container}>
+            <KeyboardAvoidingView style={main.container} behavior="padding">
 
                 <View style={main.banner}>
                     <Text style={main.todoText}>TODO</Text>
                     <Text style={main.todoCount}>10</Text>
                 </View>
 
-                <TodoInput
-                    onTodoEnter={todoText => alert(todoText)}
+                <FlatList
+                    data={list}
+                    renderItem={() => null}
                 />
 
-            </View>
+                <TodoInput
+                    onTodoEnter={todoText => addTodo(todoText)}
+                />
+
+            </KeyboardAvoidingView>
         </SafeAreaView>
     );
 }
