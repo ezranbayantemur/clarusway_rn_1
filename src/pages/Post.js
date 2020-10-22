73892a87eb19ce235cfdec8fb7f9585e902130a2
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-22 23:03:05 +0300

    Lesson completed

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, Image, Dimensions } from 'react-native';
 
 const Post = (props) => {
+
+    const postData = props.route.params.selectedPost
+
     return (
         <SafeAreaView>
             <View>
-                <Text style={{ fontSize: 50 }}>Post</Text>
+                <Text style={{ margin: 10, fontSize: 20, fontWeight: 'bold' }}>{postData.userName}</Text>
+                <Image
+                    source={{ uri: postData.img }}
+                    style={{
+                        height: Dimensions.get('window').height / 2
+                    }}
+                />
+                <Text style={{ margin: 10 }}>{postData.description}</Text>
             </View>
         </SafeAreaView>
     )
