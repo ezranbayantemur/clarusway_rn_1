6d80fd6254348fecaa53af99cc6a40c28af887d9
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-24 19:34:45 +0300

    Button added

--- src/Main.js
+++ src/Main.js
@@ -1,58 +1,16 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { SafeAreaView, View, Text, Button, FlatList } from 'react-native';
+import { SafeAreaView, View, Text, FlatList, TouchableOpacity } from 'react-native';
+
+import Button from './components/Button'
 
 const Main = (props) => {
-    const [userData, setUserData] = useState([]);
-
-    const fetchData_Then = () => {
-        axios.get('https://jsonplaceholder.typicode.com/users')
-            .then(({ data }) => {
-                setUserData(data);
-            })
-    }
-
-    const fetchData_Await = async () => {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
-        setUserData(data);
-    }
-
-
-    function promiseFunction(number) {
-        return new Promise((resolve, reject) => {
-            if (number > 5) {
-                resolve("Sayı beşten büyük!");
-            }
-            else {
-                reject("Sayı beşten küçük..");
-            }
-        })
-    }
-
-    const checkNumber = () => {
-        promiseFunction(1)
-            .then(response => {
-                console.log("response: ");
-                console.log(response);
-            })
-            .catch(error => {
-                console.log("error: ");
-                console.log(error);
-            })
-    }
 
     return (
         <SafeAreaView>
             <View>
-                <Button title="Fetch Data With Then " onPress={fetchData_Then} />
-                <Button title="Fetch Data With Await " onPress={fetchData_Await} />
-                <Button title="Number" onPress={checkNumber} />
 
-                <FlatList
-                    data={userData}
-                    renderItem={({ item }) => <Text>{item.name}</Text>}
-                />
-
+                <Button title="Suggest New Restaurant" />
             </View>
         </SafeAreaView>
     );
---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const Button = (props) => {
+    return (
+        <TouchableOpacity style={styles.container}>
+            <Text style={styles.text}>{props.title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#d7ccc8',
+        padding: 10,
+        margin: 10,
+        borderRadius: 5
+    },
+    text: {
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
+
+export default Button;
