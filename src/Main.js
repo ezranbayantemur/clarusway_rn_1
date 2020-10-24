7c43b3b7fcf614f01dc28fbb79e6a4629d91ad64
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-24 18:39:34 +0300

    Promise example

--- src/Main.js
+++ src/Main.js
@@ -17,11 +17,36 @@ const Main = (props) => {
         setUserData(data);
     }
 
+
+    function promiseFunction(number) {
+        return new Promise((resolve, reject) => {
+            if (number > 5) {
+                resolve("Sayı beşten büyük!");
+            }
+            else {
+                reject("Sayı beşten küçük..");
+            }
+        })
+    }
+
+    const checkNumber = () => {
+        promiseFunction(1)
+            .then(response => {
+                console.log("response: ");
+                console.log(response);
+            })
+            .catch(error => {
+                console.log("error: ");
+                console.log(error);
+            })
+    }
+
     return (
         <SafeAreaView>
             <View>
                 <Button title="Fetch Data With Then " onPress={fetchData_Then} />
                 <Button title="Fetch Data With Await " onPress={fetchData_Await} />
+                <Button title="Number" onPress={checkNumber} />
 
                 <FlatList
                     data={userData}
