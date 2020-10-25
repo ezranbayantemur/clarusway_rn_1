7afc585d012635d929683b852a167db34ada6702
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-25 18:48:01 +0300

    CityList page landed

--- src/Router.js
+++ src/Router.js
@@ -10,9 +10,22 @@ const Router = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Cities" component={CityList} />
-                <Stack.Screen name="Restaurants" component={RestaurantList} />
-                <Stack.Screen name="Details" component={RestaurantDetail} />
+                <Stack.Screen
+                    name="Cities"
+                    component={CityList}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Restaurants"
+                    component={RestaurantList}
+                    options={{ headerShown: false }}
+
+                />
+                <Stack.Screen
+                    name="Details"
+                    component={RestaurantDetail}
+                    options={{ headerShown: false }}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     );
--- src/pages/CityList.js
+++ src/pages/CityList.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, FlatList } from 'react-native';
 
 const CityList = (props) => {
+    const [cityList, setCityList] = useState([]);
+
+    // ASYNC-AWAIT 
+    const fetchCityData = async () => {
+        const { data } = await axios.get("https://opentable.herokuapp.com/api/cities");
+        setCityList(data.cities);
+    }
+
+    useEffect(() => {
+        fetchCityData();
+    }, [])
+
+    const renderCities = ({ item }) => <Text>{item}</Text>
+
     return (
         <SafeAreaView>
             <View>
-                <Text>CityList</Text>
+                <FlatList
+                    keyExtractor={(_, index) => index.toString()}
+                    data={cityList}
+                    renderItem={renderCities}
+                />
             </View>
         </SafeAreaView>
     )
