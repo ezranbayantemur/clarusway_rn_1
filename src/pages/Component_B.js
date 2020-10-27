917bf31586c58e7d9c7ad8064a35b89db70258b3
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-10-27 23:50:18 +0300

    Component_B created

--- src/pages/Component_B.js
+++ src/pages/Component_B.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 
-const Component_B = (props) => {
-    const ourCounter = useSelector(onur => onur.counter)
-
+const Component_B = () => {
+    const ourCounter = useSelector(myGlobalState => myGlobalState.counter);
     return (
         <View style={{ backgroundColor: '#bdbdbd', flex: 1 }}>
             <Text>Component_B</Text>
             <Text style={{ fontSize: 50 }}>Counter: {ourCounter}</Text>
-
         </View>
     );
 };
