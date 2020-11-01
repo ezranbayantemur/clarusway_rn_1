cea748950c56c8d5eab2a3db8bb93b5776251eba
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-11-01 22:00:46 +0300

    Firebase integration completed

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { SafeAreaView, View, Text, TextInput, Button } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const LoginPage = (props) => {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
 
     const signIn = () => {
-
+        auth()
+            .signInWithEmailAndPassword(email, password)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error.code);
+            })
     }
 
     const signUp = () => {
-
+        auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then((response) => {
+                //uniqe id
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     return (
