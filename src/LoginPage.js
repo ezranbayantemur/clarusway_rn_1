d1eb355df1aa324026f356e49d9c45853e2b2087
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-11-01 21:15:27 +0300

    Login page created

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, TextInput, Button } from 'react-native';
 
 const LoginPage = (props) => {
     return (
         <SafeAreaView>
             <View>
-                <Text>Login</Text>
+                <Text style={{ fontWeight: 'bold', fontSize: 20, textAlign: 'center' }}>Login</Text>
+
+                <View style={{ backgroundColor: '#eceff1', padding: 5, margin: 5, borderRadius: 5 }}>
+                    <TextInput
+                        placeholder="Type your mail.."
+                    />
+                </View>
+
+                <View style={{ backgroundColor: '#eceff1', padding: 5, margin: 5, borderRadius: 5 }}>
+                    <TextInput
+                        placeholder="Type your password"
+                    />
+                </View>
+
+                <Button
+                    title="Sign Up"
+                />
+                <Button
+                    title="Sign In"
+                />
+
             </View>
         </SafeAreaView>
     );
 }
 
-export default LoginPage;
+export default LoginPage;   
