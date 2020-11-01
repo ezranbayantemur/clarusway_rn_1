5bc80d8f396dd064979f95f9d29393edef23059d
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-11-01 21:18:29 +0300

    Finished login interface

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { SafeAreaView, View, Text, TextInput, Button } from 'react-native';
 
 const LoginPage = (props) => {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    const signIn = () => {
+
+    }
+
+    const signUp = () => {
+
+    }
+
     return (
         <SafeAreaView>
             <View>
@@ -10,20 +21,24 @@ const LoginPage = (props) => {
                 <View style={{ backgroundColor: '#eceff1', padding: 5, margin: 5, borderRadius: 5 }}>
                     <TextInput
                         placeholder="Type your mail.."
+                        onChangeText={text => setEmail(text)}
                     />
                 </View>
 
                 <View style={{ backgroundColor: '#eceff1', padding: 5, margin: 5, borderRadius: 5 }}>
                     <TextInput
                         placeholder="Type your password"
+                        onChangeText={text => setPassword(text)}
                     />
                 </View>
 
                 <Button
                     title="Sign Up"
+                    onPress={signUp}
                 />
                 <Button
                     title="Sign In"
+                    onPress={signIn}
                 />
 
             </View>
